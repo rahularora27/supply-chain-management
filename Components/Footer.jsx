85aa9af9eff6da264e6007a861759585f62f0546
Rahul Arora <rahularora2715@gmail.com>
2024-05-15 14:24:04 +0530

    did stuff

---
+++ Components/Footer.jsx
@@ -0,0 +1,9 @@
+export default () => {
+  return (
+    <footer className="pt-10">
+        <div className="mt-10 py-10 border-t md:text-center">
+          <p>© 2024 Rahul Arora. All rights reserved.</p>
+        </div>
+    </footer>
+  );
+};
