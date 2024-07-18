7790130f7763adec4e26e90122d3f5b149b40bf2
Rahul Arora <rahularora2715@gmail.com>
2024-07-19 01:43:38 +0530

    redo

--- Components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default () => {
-  return (
-    <footer>
-        <div className="mt-64 py-4 border-t text-center">
-          <p>© 2024 Rahul Arora. All rights reserved.</p>
-        </div>
-    </footer>
-  );
-};
